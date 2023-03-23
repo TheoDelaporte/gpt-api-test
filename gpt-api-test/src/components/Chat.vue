dd8c74601f99e655d7b4203de8b3f617d5fec6ce
Theo delaporte <theodelaporte@MacBook-Pro-de-Theo.local>
2023-03-23 18:06:44 +0100

    WIP - new design on home page

--- gpt-api-test/src/components/Chat.vue
+++ gpt-api-test/src/components/Chat.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="chat" ref="currentMessage">
-    <div class="profile">
-      <img v-if="chat.isAi" src="@/assets/bot.svg" alt="" />
-      <img v-else src="@/assets/user.svg" alt="" />
-    </div>
-    <div class="message">
-      {{ chat.value }}
+  <div class="chat-wrapper">
+    <div class="message-wrapper reverse">
+      <img class="message-pp" v-if="chat.isAi" src="@/assets/bot.svg" alt=""/>
+      <img class="message-pp" v-else src="@/assets/user.svg" alt=""/>
+      <div class="message-box-wrapper">
+        <div class="message-box">
+          {{ chat.value }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
-  
-  <script setup>
-import { nextTick, onMounted, ref, watch } from "vue";
+
+<script setup>
+import {nextTick, onMounted, ref, watch} from "vue";
+
 defineProps({
   chat: {
     type: Object || null,
@@ -21,9 +24,9 @@ const currentMessage = ref(null);
 onMounted(async () => {
   await nextTick();
   const div = currentMessage.value;
-  div.scrollIntoView({ behavior: "smooth" });
+  div.scrollIntoView({behavior: "smooth"});
 });
 </script>
-  
-  <style lang="scss" scoped>
+
+<style lang="scss" scoped>
 </style>
